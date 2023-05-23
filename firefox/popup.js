a04c84783d30cec36a4fe748df4b32c5f31857b4
yungsamd17 <64147848+yungsamd17@users.noreply.github.com>
2023-05-23 18:08:07 +0200

    🔧Update `firefox` to v1.1.3 same as `chrome`

--- firefox/popup.js
+++ firefox/popup.js
@@ -6,10 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
       return; // exit the function if the username is empty
     }
     let url = "https://player.twitch.tv/?channel=" + username + "&parent=twitch.tv";
-    chrome.tabs.create({ url: url });
-    window.close();
+    openUrl(url);
   });
-  document.getElementById("username").focus();
 
   document.getElementById("chatButton").addEventListener("click", function (event) {
     event.preventDefault();
@@ -18,18 +16,28 @@ document.addEventListener("DOMContentLoaded", function () {
       return; // exit the function if the username is empty
     }
     let url = "https://www.twitch.tv/popout/" + username + "/chat";
-    chrome.tabs.create({ url: url });
-    window.close();
+    openUrl(url);
   });
 
   document.getElementById("channelButton").addEventListener("click", function (event) {
     event.preventDefault();
     let username = document.getElementById("username").value.trim();
     if (username === '') {
-      return; // exit the function of the username it empty
+      return; // exit the function if the username is empty
     }
     let url = "https://twitch.tv/" + username;
-    chrome.tabs.create({ url: url });
-    window.close();
+    openUrl(url);
   });
+
+  function openUrl(url) {
+    browser.tabs.query({ active: true, currentWindow: true }).then(function(tabs) {
+      const newTab = document.getElementById("openNewTabCheckbox").checked;
+      if (newTab) {
+        browser.tabs.create({ url: url });
+      } else {
+        browser.tabs.update(tabs[0].id, { url: url });
+      }
+      window.close();
+    });
+  }
 });

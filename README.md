dcde7a484541db52d0a4c161d435ea6ffcc9929b
yungsamd17 <yungsamd@proton.me>
2024-01-21 20:47:29 +0100

    ⬆️Update v.1.2.0 - Manifest V3

--- README.md
+++ README.md
@@ -1,29 +1,26 @@
 # <img src="extension/src/icons/icon48.png" width="38" align="left" /> Open Twitch Channel
 
-Type channel username and go to [player.twitch.tv](https://player.twitch.tv/?channel=xqc&parent=twitch.tv) with just pressing **ENTER**. Or Channel at [twitch.tv](https://twitch.tv/xqc) and Chat at [twitch.tv/popout](https://www.twitch.tv/popout/xqc/chat).
+Type channel username and go to Channel with just pressing **ENTER**. Or Player and Chat at.
 
-- Better and cleaner way to watch Twitch.
-- Best for users who mainly just use [Chatterino](https://chatterino.com/) for chat.
-- Better viewing experience for people on lower end systems. ***(no/less dropped frames than normal [Twitch](https://twitch.tv))***
+### Features
+
+- Open streams with Channel, Player and Chat.
+- Go to channel with just pressing **ENTER**.
 - Open in New/Active tab checkbox.
 
-![screenshot](https://github.com/yungsamd17/Open-Twitch-Channel/assets/64147848/042b869a-bf58-4cae-bb36-7c33b22fca12)
+## Install
 
-# Download
-### [**Firefox**](https://addons.mozilla.org/en-US/firefox/addon/open-twitch-channel/)
-### [**Chrome**](https://github.com/yungsamd17/Open-Twitch-Channel/releases/latest/download/open-twitch-channel.zip) (*Download and follow instructions*)
+_Chrome extension not published yet._
+<br>
+<a href="CHROME_WEB_STORE_LINK">
+	<img src="https://raw.githubusercontent.com/alrra/browser-logos/main/src/chrome/chrome.svg" width="48" alt="Chrome" valign="middle"> <b>Chrome</b>
+</a>
+or any chromium-based browsers (e.g. Brave, Edge, Opera)
+<br>
+<a href="https://addons.mozilla.org/en-US/firefox/addon/open-twitch-channel/">
+	<img src="https://raw.githubusercontent.com/alrra/browser-logos/main/src/firefox/firefox.svg" width="48" alt="Firefox" valign="middle"> <b>Firefox</b>
+</a>
 
-## Installing for Chrome
-Here is a short guide how to install an unpacked extension on Google Chrome or Chromium-based browsers (e.g. Brave, Edge, Opera).
+## License
 
-1. Unzip the folder.
-
-2. Place the folder somewhere you might not remove it by mistake.
-
-3. Open the **"Extensions"** page *`chrome://extensions/`* in the browser and turn on the **"Developer mode"**.
-
-![screenshot2](https://user-images.githubusercontent.com/64147848/228734049-1e16ee90-804b-4412-bd86-c799dda84d77.png)
-
-4. Click on the **"Load unpacked"** button and select the directory where the extension is placed.
-
-5. Done, Enjoy.
+This software is released under the terms of the MIT License. See the [LICENSE](https://github.com/yungsamd17/Open-Twitch-Channel/blob/main/LICENSE) for further information.
--- extension/popup.js
+++ extension/popup.js
@@ -1,62 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const openNewTabCheckbox = document.getElementById("openNewTabCheckbox");
+document.addEventListener("DOMContentLoaded", function() {
+    const openNewTabCheckbox = document.getElementById("openNewTabCheckbox");
+    const usernameInput = document.getElementById("username");
 
-  // Retrieve the checkbox state from storage and update the checkbox on popup load
-  chrome.storage.local.get(["openNewTabCheckbox"], function (result) {
-    openNewTabCheckbox.checked = result.openNewTabCheckbox || false;
-  });
-
-  document.getElementById("playerButton").addEventListener("submit", function (event) {
-    event.preventDefault();
-    let username = document.getElementById("username").value.trim();
-    if (username === '') {
-      return; // exit the function if the username is empty
-    }
-    let url = "https://player.twitch.tv/?channel=" + username + "&parent=twitch.tv";
-    openUrl(url);
-  });
-
-  document.getElementById("chatButton").addEventListener("click", function (event) {
-    event.preventDefault();
-    let username = document.getElementById("username").value.trim();
-    if (username === '') {
-      return; // exit the function if the username is empty
-    }
-    let url = "https://www.twitch.tv/popout/" + username + "/chat";
-    openUrl(url);
-  });
-
-  document.getElementById("channelButton").addEventListener("click", function (event) {
-    event.preventDefault();
-    let username = document.getElementById("username").value.trim();
-    if (username === '') {
-      return; // exit the function if the username is empty
-    }
-    let url = "https://twitch.tv/" + username;
-    openUrl(url);
-  });
-
-  // Save the checkbox state to storage when it changes
-  openNewTabCheckbox.addEventListener("change", function () {
-    const isChecked = openNewTabCheckbox.checked;
-
-    chrome.storage.local.set({ openNewTabCheckbox: isChecked }, function () {
-      console.log("Open Twitch Channel - Checkbox state saved.");
+    // Retrieve the checkbox state from storage and update the checkbox on popup load
+    chrome.storage.local.get({ "openNewTabCheckbox": false }, function(result) {
+        openNewTabCheckbox.checked = result.openNewTabCheckbox;
     });
-  });
 
-  function openUrl(url) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.storage.local.get(["openNewTabCheckbox"], function (result) {
-        const newTab = result.openNewTabCheckbox || false;
+    function openUrl(url) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            const newTab = openNewTabCheckbox.checked;
 
-        if (newTab) {
-          chrome.tabs.create({ url: url });
-        } else {
-          chrome.tabs.update(tabs[0].id, { url: url });
-        }
-        window.close();
-      });
+            if (newTab) {
+                chrome.tabs.create({ url: url });
+            } else {
+                chrome.tabs.update(tabs[0].id, { url: url });
+            }
+            window.close();
+        });
+    }
+
+    function handleButtonClick(urlBuilder) {
+        return function(event) {
+            event.preventDefault();
+            const username = usernameInput.value.trim();
+            if (username === '') {
+                return; // exit the function if the username is empty
+            }
+            const url = urlBuilder(username);
+            openUrl(url);
+        };
+    }
+
+    // Button event listeners
+    document.getElementById("channelButton").addEventListener("submit", handleButtonClick(username => `https://twitch.tv/${username}`));
+    document.getElementById("playerButton").addEventListener("click", handleButtonClick(username => `https://player.twitch.tv/?channel=${username}&parent=twitch.tv`));
+    document.getElementById("chatButton").addEventListener("click", handleButtonClick(username => `https://www.twitch.tv/popout/${username}/chat`));
+
+    // Save the checkbox state to storage when it changes
+    openNewTabCheckbox.addEventListener("change", function() {
+        const isChecked = openNewTabCheckbox.checked;
+
+        chrome.storage.local.set({ "openNewTabCheckbox": isChecked }, function() {
+            console.log("Open Twitch Channel - Checkbox state saved.");
+        });
     });
-  }
 });
